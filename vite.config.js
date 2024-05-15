048b4289e147a13eae868021792930aba1ae1996
omkarjagtap2212 <omkarjagtap2312@gmail.com>
2024-05-15 22:05:50 +0530

    changes inside the vite.config file

--- vite.config.js
+++ vite.config.js
@@ -4,14 +4,16 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: "dist"
-  },
+ 
   server: {
     watch: {
       usePolling: true,
     },
 
   },
+  build: {
+    outDir: "dist"
+  },
+
 
 })
